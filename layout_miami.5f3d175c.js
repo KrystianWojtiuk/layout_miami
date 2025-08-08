52ecd9659f8cd657db7a6cfc10fb9be1cf643a33
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-08-09 00:08:13 +0100

    make build

---
+++ layout_miami.5f3d175c.js
@@ -0,0 +1,2 @@
+"use strict";window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.documentElement.classList.add("html--with-menu"):document.documentElement.classList.remove("html--with-menu")});
+//# sourceMappingURL=layout_miami.5f3d175c.js.map
